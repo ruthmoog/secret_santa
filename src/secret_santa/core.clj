8c88d6371e67ae00e798aefff38d9543c144c6aa
Ruth Baker <ruthmoog@ruths-mbp.home>
2017-12-02 19:55:25 +0000

    clarified bindings (variable names)

--- src/secret_santa/core.clj
+++ src/secret_santa/core.clj
@@ -4,10 +4,10 @@
 (def my-santas ["Joey" "Phoebe" "Ross" "Chandler" "Monica" "Rachel"])
 
 (defn secret-santa [] (let [
-  randomised-santas (shuffle my-santas)
-  shifted-santas (cons (last randomised-santas) (butlast randomised-santas))
+  shuffled (shuffle my-santas)
+  shifted (cons (last shuffled) (butlast shuffled))
   ]
-  (zipmap randomised-santas shifted-santas)))
+  (zipmap shuffled shifted)))
 
 ;; boilerplate, leave me alone
 (defn -main
